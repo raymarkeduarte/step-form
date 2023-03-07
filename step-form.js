c0ef702db4d7f4fb7734374baf82bed072827d2a
Raymark <raymarkedelacruz@gmail.com>
2023-03-07 11:46:09 +0800

    trigger active or disabled class for the form step menu

--- step-form.js
+++ step-form.js
@@ -1,11 +1,19 @@
+/**
+ * @description hide current form then shows the target
+ * @author RED
+ */
 $(document).on('click', '[target-form-step]', function(){
     const container = $(this).closest('[form-step]');
     const selector = $(this).attr('target-form-step');
-    const target = $('[form-step='+selector+']');
+    const target = $('[form-step="'+selector+'"]');
+    const targetMenu = $('[form-step-menu="'+selector+'"]');
+    const currentMenu = $('[form-step-menu="'+container.attr('form-step')+'"]');
+    currentMenu.removeClass('active');
+    currentMenu.addClass('disabled');
+    targetMenu.removeClass('disabled');
+    targetMenu.addClass('active');
     container.hide();
     target.show();
-    $('body').css('background', randomHexColor());
-    $('h1').css('color', randomHexColor());
 });
 function randomHexColor(){
     return '#'+Math.floor(Math.random()*16777215).toString(16);
